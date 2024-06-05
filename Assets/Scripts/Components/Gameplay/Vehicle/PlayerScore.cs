766c175d601f0b5256b17198403fa15d11037d94
emaldonadogamedev <esteban.maldonado@unity3D.com>
2024-06-05 15:37:33 -0700

    preparing for workshop
    "breaking" the game on purpose for presentation purposes

--- Assets/Scripts/Components/Gameplay/Vehicle/PlayerScore.cs
+++ Assets/Scripts/Components/Gameplay/Vehicle/PlayerScore.cs
@@ -9,10 +9,21 @@ namespace Unity.MegacityMetro.Gameplay
     /// </summary>
     public struct PlayerScore : IComponentData
     {
-        [GhostField(Quantization = 1000)] public float Value;
+        // [GhostField] is an attribute from Netcode for Entities that marks a field
+        //  as one that should be synchronized across the network
+
+        //[GhostField(Quantization = 1000)]
+        public float Value;
+        
         public bool IsLocalPlayer;
-        [GhostField] public int Kills;
-        [GhostField] public FixedString64Bytes KilledPlayer;
-        [GhostField] public FixedString64Bytes KillerName;
+        
+        //[GhostField]
+        public int Kills;
+        
+        //[GhostField]
+        public FixedString64Bytes KilledPlayer;
+        
+        //[GhostField]
+        public FixedString64Bytes KillerName;
     }
 }
--- Assets/Scripts/Components/Gameplay/Vehicle/VehicleHealth.cs
+++ Assets/Scripts/Components/Gameplay/Vehicle/VehicleHealth.cs
@@ -8,9 +8,15 @@ namespace Unity.MegacityMetro.Gameplay
     /// </summary>
     public struct VehicleHealth : IComponentData
     {
-        [GhostField(Quantization = 1000)] public float Value;
-        [GhostField] public byte IsDead;
-        [GhostField] public float LookAtEnemyDegrees;
+        [GhostField(Quantization = 1000)]
+        public float Value;
+        
+        [GhostField]
+        public byte IsDead;
+        
+        [GhostField]
+        public float LookAtEnemyDegrees;
+        
         public float _deathTimer;
         public byte _DeathFXState;
     }
